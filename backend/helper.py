f82ba192c6c70ce069198e4d51a3926656a694b1
Eyiza <Precious.michael2002@gmail.com>
2023-09-23 01:09:54 +0100

    updated currency converter

--- backend/helper.py
+++ backend/helper.py
@@ -11,18 +11,25 @@ def currency_symbol(currency_code):
 
 
 def convert_currency(amount, from_currency, to_currency):
-    url = 'https://api.exchangerate.host/convert'
-    params = {
-        'from': from_currency,
-        'to': to_currency,
+    # url = 'https://cdn.moneyconvert.net/api/latest.json'    
+    # payload = {
+    #     'from': from_currency,
+    #     'to': to_currency,
+    # } 
+
+    url = f"https://api.apilayer.com/fixer/convert?to={to_currency}&from={from_currency}&amount={amount}" 
+    payload = {}
+    headers = {
+        "apikey": os.environ.get("API_KEY")
     }
-    response = requests.get(url, params=params)
-    # print(response)
+    response = requests.get(url, headers=headers, data = payload)
     # print("Request URL:", response.url)
     if response.ok:
         data = response.json()
-        conversion_rate = Decimal(data['result'])
+        # conversion_rate = Decimal(data['result'])
         # conversion_rate = Decimal(str(data['result']))
-        converted_amount = amount * conversion_rate
+        # converted_amount = amount * conversion_rate
+
+        converted_amount = Decimal(data['result'])
         return round(converted_amount, 2)
     return None
